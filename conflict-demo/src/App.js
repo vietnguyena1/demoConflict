78aa14fc08002ef006c3e9b30cecf39445b3a84a
vietnguyena1 <35227782+vietnguyena1@users.noreply.github.com>
2022-10-10 07:27:46 +0700

    f1-3

--- conflict-demo/src/App.js
+++ conflict-demo/src/App.js
@@ -2,14 +2,14 @@ import {useState} from 'react';
 import './App.css';
 
 function App() {
-  [count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
       <header className="App-header">
       <div class="container">
             <ul>
-              <li style={{ '--i': 6 }} onMouseOver={()=>{count+1}}>{count}</li>          
+              <li style={{ '--i': 6 }} onMouseOver={()=>{setCount(count+1)}}>{count}</li>          
             </ul>
           </div>
       </header>
